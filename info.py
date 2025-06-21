1507aa017a0d7670b3f6d9d09f74a7e1ab2c7aa4
Sagar787898 <jasminekidsindia@gmail.com>
2025-06-21 15:18:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,11 +21,11 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7878505422:AAGhhcUQQZ_BtRr8GXSAPtA1vltOQO7
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5424912278').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/srcinemahub") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002537625474'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+vHh_UbhKUy4yNGM1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sagar000:Sagar000@cluster0.1fewng5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
 DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
